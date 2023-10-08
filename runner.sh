71a51b747e57b148109620dbdd0e2c852b0d2ae5
ethandmd <ethandmd@protonmail.com>
2023-10-08 00:18:11 -0700

    Simplify runner

--- runner.sh
+++ runner.sh
@@ -11,10 +11,5 @@ cp test_ioctl busybox/_install/
 cd busybox/_install/
 find . | cpio -H newc -o | gzip > ../ramdisk.img.gz
 
-cd ../..
-cp min-sample-x86.config linux/.config
-cd linux/
-make LLVM=1 -j$(nproc)
-
-cd ../
+cd ../../
 qemu-system-x86_64 -enable-kvm -kernel linux/arch/x86/boot/bzImage -initrd busybox/ramdisk.img.gz -nographic
